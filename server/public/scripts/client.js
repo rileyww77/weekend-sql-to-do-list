37f1fb2ba3c35b71a5177c06495802656b6aad89
Riley Webster <riley_ww77@yahoo.com>
2020-08-07 16:41:56 -0500

    added inputs and buttons, added initial get request on client side

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,5 +3,29 @@ console.log('client is running');
 $(document).ready(onReady);
 
 function onReady(){
+    //click events
+    $('#submitButton').on('click', addNewTask);
+
+    //getting the tasks on the DOM
+    getTasks();
+}
+
+//GET request, puts tasks on DOM
+function getTasks(){
+console.log('in getTasks');
+
+$.ajax({
+    type: 'GET',
+    url: '/tasks'
+}).then(function (response){
+    $('#newTasks').empty();
+    console.log(response);
+})
     
+}
+
+
+//adding new task, will be PUT request
+function addNewTask(){
+    console.log('clicked');
 }
